efba56f4dd39d4d9a64a6100d1012acddc39cd11
S <323@323-11>
2022-09-06 10:32:53 +0300

    Added shmaded

--- ProductStoreApp/Controllers/CategoryController.cs
+++ ProductStoreApp/Controllers/CategoryController.cs
@@ -1,21 +1,76 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿
+using Microsoft.AspNetCore.Mvc;
 using ProductStoreApp.Data;
 using ProductStoreApp.Models;
 
-namespace ProductStoreApp.Controllers
+namespace FoodStore.Controllers
 {
     public class CategoryController : Controller
     {
-       private readonly ApplicationDbContext _db;
-       public CategoryController(ApplicationDbContext db)
+        // Переменная контекста для доступа к базе данных
+        private readonly ApplicationDbContext _db;
+
+        // Внедряем зависимость ApplicationDbContext через конструктор
+        public CategoryController(ApplicationDbContext db)
         {
-            this._db = db;
+            _db = db;
         }
 
         public IActionResult Index()
         {
+            // Создаем список записей категорий из базы данных
             IEnumerable<Category> categoryList = _db.Category;
+            // Возвращаем загруженный список в представление
             return View(categoryList);
+        }
+
+        //GET - CREATE
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        //POST - CREATE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category cat)
+        {
+
+            _db.Category.Add(cat);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        //GET - EDIT
+        public IActionResult Edit(int? id)
+        {
+            return View();
+        }
+
+
+        //GET - DELETE
+        public IActionResult Delete(int? id)
+        {
+            return View();
+        }
+
+        //POST - DELETE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var cat = _db.Category.Find(id);
+            if (cat == null)
+            {
+                return NotFound();
+            }
+            _db.Category.Remove(cat);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+
+
         }
     }
 }
