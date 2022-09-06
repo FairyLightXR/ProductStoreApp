93ebaade5ac4798c55f782e2f7f46f3354abeb3e
S <323@323-11>
2022-09-06 10:24:57 +0300

    Добавьте файлы проекта.

---
+++ ProductStoreApp/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductStoreApp.Data;
+using ProductStoreApp.Models;
+
+namespace ProductStoreApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+       private readonly ApplicationDbContext _db;
+       public CategoryController(ApplicationDbContext db)
+        {
+            this._db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> categoryList = _db.Category;
+            return View(categoryList);
+        }
+    }
+}
---
+++ ProductStoreApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using ProductStoreApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace ProductStoreApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(
+           DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
+        public DbSet<Category> Category { get; set; }
+    }
+}
